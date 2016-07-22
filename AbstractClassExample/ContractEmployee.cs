e398f6dea4239e92c608635bc9fff063918e4015
djou <djou123@yahoo.com>
2016-07-21 22:13:54 -0500

    created class

---
+++ AbstractClassExample/ContractEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExample
+{
+    public class ContractEmployee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int HourlyPay { get; set; }
+        public int TotalHour { get; set; }
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public int GetMonthlySalary()
+        {
+            return this.TotalHour * this.HourlyPay;
+        }
+    }
+}
--- AbstractClassExample/Program.cs
+++ AbstractClassExample/Program.cs
@@ -8,8 +8,12 @@ namespace AbstractClassExample
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            FullTimeEmployee fte = new FullTimeEmployee();
+            ID = 101;
+            FirstNmae
         }
-    }
+     }
+
 }
